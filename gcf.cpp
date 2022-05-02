a6a90519bc297af4a95dee61a1ca9ce446733b47
Ubuntu <ubuntu@ip-172-31-91-254.ec2.internal>
2022-05-02 23:40:50 +0000

    Made a program that calculates the gcf of two given numbers using a for loop. It then asks the user if they would like to play again.

--- gcf.cpp
+++ gcf.cpp
@@ -3,12 +3,12 @@
 // Created by: Sarah
 // Created on: May 1st, 2022.
 // This program asks the user to enter two positive numbers. It then calculates
-// the gcf of those two numbers using a foor loop. It also asks the user if
+// the gcf of those two numbers using a for loop. It also asks the user if
 // they would like to play again.
 #include <iostream>
 #include <iomanip>
 #include <string>
-#include <sstream>
+
 
 
 int askAgain() {
@@ -17,7 +17,7 @@ int askAgain() {
     std::cout << "Would you like to play again (y/n): ";
     std::cin >> askToPlay;
     if (askToPlay == "y" || askToPlay == "Y") {
-        // state varaibles
+        // declare variables & strings.
         int counter;
         int userNum;
         int userNum2;
@@ -34,88 +34,94 @@ int askAgain() {
         std::cin >> userNum2String;
 
         try {
-        // convert the user entered to integer
-        userNum = std::stoi(userNumString);
+            // convert userNum to integer
+            userNum = std::stoi(userNumString);
 
-        try {
-            userNum2 = std::stoi(userNum2String);
+            try {
+                // convert userNum2 to integer
+                userNum2 = std::stoi(userNum2String);
 
-            // sets a range.
-            if (userNum > 0) {
-                if (userNum2 >= 0) {
-                // calculate the gcf of the two numbers user entered.
-                    for (counter = 1; counter <= userNum2; ++counter) {
-                        if (userNum % counter == 0 && userNum2 % counter == 0) {
-                            gcf = counter;
+                // sets a range.
+                if (userNum > 0) {
+                    if (userNum2 >= 0) {
+                        // calculate the gcf of the two numbers user entered &
+                        // then it displays it.
+                        for (counter = 1; counter <= userNum2; ++counter) {
+                            if (userNum % counter == 0
+                                && userNum2 % counter == 0) {
+                                gcf = counter;
+                            }
                         }
-                    }
-                    std::cout << "The GCF of " << userNum << " and ";
-                    std::cout << userNum2 << " is " << gcf << ".\n";
-                    std::cout << std::endl;
-                    askAgain();
+                        std::cout << "The GCF of " << userNum << " and ";
+                        std::cout << userNum2 << " is " << gcf << ".\n";
+                        std::cout << std::endl;
+                        askAgain();
 
-                    return 0;
+                        return 0;
+                    } else {
+                        std::cout << "Please enter a whole number.";
+                        std::cout << "" << std::endl;
+                    }
                 } else {
                     std::cout << "Please enter a whole number.";
                     std::cout << "" << std::endl;
                 }
-            } else {
-                std::cout << "Please enter a whole number.";
-                std::cout << "" << std::endl;
-            }
 
-            // handles error case.
+                // handles error case.
             } catch (std::invalid_argument) {
                 std::cout << userNum2String << " is not a valid number.\n";
                 std::cout << "" << std::endl;
             }
-        // handles the error case.
+            // handles the error case.
         } catch (std::invalid_argument) {
-        // The user did not enter an integer.
-        std::cout << userNumString << " is not a valid number.\n";
-        std::cout << "" << std::endl;
+            // The user did not enter an integer.
+            std::cout << userNumString << " is not a valid number.\n";
+            std::cout << "" << std::endl;
     }
     askAgain();
     } else if (askToPlay == "n" || askToPlay == "N") {
         std::cout << "Thanks for playing! \n";
     } else {
-      std::cout << "Please enter either or being (y/n): \n";
+      std::cout << "Please enter either or being (y/n). \n";
       askAgain();
     }
 }
 
-using namespace std;
+
 
 int main() {
-  // declare variables & strings
-  int counter;
-  int gcf;
-  int userNum2;
-  int userNum;
-  std::string userNumString;
-  std::string userNum2String;
+    // declare variables & strings
+    int counter;
+    int gcf;
+    int userNum2;
+    int userNum;
+    std::string userNumString;
+    std::string userNum2String;
 
-  // get the user number as a string
-  std::cout << "Today, we will be finding the GCF of two numbers.\n";
-  std::cout << std::endl;
-  std::cout << "Enter the first whole number: ";
-  std::cin >> userNumString;
-  std::cout << "Enter the second whole number: ";
-  std::cin >> userNum2String;
+    // get the user number as a string
+    std::cout << "Today, we will be finding the GCF of two numbers.\n";
+    std::cout << std::endl;
+    std::cout << "Enter the first whole number: ";
+    std::cin >> userNumString;
+    std::cout << "Enter the second whole number: ";
+    std::cin >> userNum2String;
 
-  try {
-        // convert the user entered to integer
+    try {
+        // convert userNum entered to integer
         userNum = std::stoi(userNumString);
 
         try {
+            // converts userNum2 into an integer
             userNum2 = std::stoi(userNum2String);
 
             // sets a range.
             if (userNum > 0) {
                 if (userNum2 >= 0) {
-                // calculate the gcf of the two numbers user entered.
+                // calculate the gcf of the two numbers user entered & then it
+                // displays it.
                     for (counter = 1; counter <= userNum2; ++counter) {
-                        if (userNum % counter == 0 && userNum2 % counter == 0) {
+                        if (userNum % counter == 0
+                            && userNum2 % counter == 0) {
                             gcf = counter;
                         }
                     }
@@ -134,16 +140,16 @@ int main() {
                 std::cout << "" << std::endl;
             }
 
-            // handles error case.
-            } catch (std::invalid_argument) {
-                std::cout << userNum2String << " is not a valid number.\n";
-                std::cout << "" << std::endl;
-            }
-        // handles the error case.
+        // handles error case.
         } catch (std::invalid_argument) {
-        // The user did not enter an integer.
-        std::cout << userNumString << " is not a valid number.\n";
-        std::cout << "" << std::endl;
-    }
-askAgain();
+          std::cout << userNum2String << " is not a valid number.\n";
+          std::cout << "" << std::endl;
+          }
+        // handles the error case.
+    } catch (std::invalid_argument) {
+      // The user did not enter an integer.
+      std::cout << userNumString << " is not a valid number.\n";
+      std::cout << "" << std::endl;
+      }
+    askAgain();
 }
